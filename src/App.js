15630d3d21af090216502e4607f9cd0aee093c0c
karishmamohammed <karishmamohammed43@gmail.com>
2023-01-16 14:44:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import TypingBox from "./Components/TypingBox.jsx";
+import { GlobalStyles } from "./Styles/global.js";
+
+var randomWords =  require('random-words');
 
 function App() {
+
+  const words = randomWords(100);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="canvas">
+
+      <GlobalStyles/>
+      <h1>Typing Test</h1>
+      <TypingBox words={words}/>
+      <h1>Footer</h1>
+   </div>
   );
 }
 
